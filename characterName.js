63f65ecc0680d93c615a225c0d2ff7ce752f47b9
henniyoon <2749126@naver.com>
2023-11-07 00:23:30 +0900

    별빛 전체 길드원명 직위순 크롤링 후 mainCharacterName 배열에 저장

--- characterName.js
+++ characterName.js
@@ -1,35 +1,41 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require("axios");
+const cheerio = require("cheerio");
 const mainCharacterName = [];
-
-// 크롤링할 웹 페이지의 URL을 지정합니다.
-for (let i = 1; i <= 10; i++) {
-    url = `https://maplestory.nexon.com/Common/Guild?gid=402812&wid=0&orderby=1&page=${i}`;
-    scrapeRankTable(url);
-}
+const numPages = 10; // 크롤링할 페이지 수
 
 async function scrapeRankTable(url) {
-    try {
-        const response = await axios.get(url);
-        const html = response.data;
-        const $ = cheerio.load(html);
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-        // class가 rank_table인 표를 식별합니다.
-        const rankTable = $('table.rank_table');
+    // class가 rank_table인 표를 식별합니다.
+    const rankTable = $("table.rank_table");
 
-        // 표에서 a 태그 안의 정보를 추출
-        rankTable.find('tr').each((rowIndex, row) => {
-            
-            $(row).find('td a').each((colIndex, link) => {
-                mainCharacterName.push($(link).text().trim());
-            });
-            // 데이터 출력
-            console.log(mainCharacterName); 
+    // 표에서 a 태그 안의 정보를 추출
+    rankTable.find("tr").each((rowIndex, row) => {
+      $(row)
+        .find("td a")
+        .each((colIndex, link) => {
+          mainCharacterName.push($(link).text().trim());
         });
-
-    } catch (error) {
-        console.error('오류 발생:', error);
-    }
+    });
+  } catch (error) {
+    console.error("오류 발생:", error);
+  }
 }
 
+async function scrapeData() {
+  const promises = [];
 
+  for (let i = 1; i <= numPages; i++) {
+    const url = `https://maplestory.nexon.com/Common/Guild?gid=402812&wid=0&orderby=1&page=${i}`;
+    promises.push(scrapeRankTable(url));
+  }
+
+  await Promise.all(promises);
+
+  console.log(mainCharacterName);
+}
+
+scrapeData();
