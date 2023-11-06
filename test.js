228dd98d0fa0ed2b9969306fd30e50e8879858a7
henniyoon <2749126@naver.com>
2023-11-07 00:14:43 +0900

    별빛 전체 길드원명 직위순 크롤링

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const mainCharacterName = [];
 
 // 크롤링할 웹 페이지의 URL을 지정합니다.
 for (let i = 1; i <= 10; i++) {
-    url = `https://maplestory.nexon.com/Common/Guild?gid=402812&wid=0&orderby=0&page=${i}`;
+    url = `https://maplestory.nexon.com/Common/Guild?gid=402812&wid=0&orderby=1&page=${i}`;
     scrapeRankTable(url);
 }
 
@@ -17,17 +18,15 @@ async function scrapeRankTable(url) {
         const rankTable = $('table.rank_table');
 
         // 표에서 a 태그 안의 정보를 추출
-        const tableData = [];
         rankTable.find('tr').each((rowIndex, row) => {
-            const rowData = [];
+            
             $(row).find('td a').each((colIndex, link) => {
-                rowData.push($(link).text().trim());
+                mainCharacterName.push($(link).text().trim());
             });
-            tableData.push(rowData);
+            // 데이터 출력
+            console.log(mainCharacterName); 
         });
 
-        // 데이터 출력
-        console.log('크롤링한 데이터:', tableData);
     } catch (error) {
         console.error('오류 발생:', error);
     }
