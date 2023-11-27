66bc62517bf6b879cbc0f54136d1a89b5b079d08
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2023-11-27 22:20:05 +0900

    DB 값을 불러오는 코드 작성

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const path = require('path');
+const mariadb = require('mariadb');
 
 const app = express();
 const port = 3030; // 사용할 포트 번호
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '0000',
+  database: 'guild_manager'
+});
 
 // public 폴더 내의 정적 파일 (index.html)을 제공
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/members', async (req, res) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query('SELECT * FROM main_member');
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  } finally {
+    if (conn) conn.release();
+  }
+});
 
 // 서버를 시작
 app.listen(port, () => {
